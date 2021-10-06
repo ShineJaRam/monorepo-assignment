d02d873ea620dbda031674b956350fd9ff707380
임수현 <suhuyn19@naver.com>
2021-10-06 11:06:05 +0900

    feat(my-app2): lerna-scope 에 맞는 commit message 를 작성 및 실습해봅니다

--- packages/my-app2/src/App.tsx
+++ packages/my-app2/src/App.tsx
@@ -7,18 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Learn Monorepo in my-app2
+        <br />
+        <br />
       </header>
+      @commitlint/config-lerna-scopes 룰에 맞는 commit 메세지 작성을 하는 연습
+      중입니다. scope에 맞는 commit message를 작성해봅니다.
     </div>
   );
 }
